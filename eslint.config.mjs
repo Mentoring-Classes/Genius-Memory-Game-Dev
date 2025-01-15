a80b480524e813c7657e10bc200bc859e440bc79
Daniel Obara <daniel.tsutomu@hotmail.com>
2025-01-15 14:09:37 -0300

    fix: eslint script at package to be used in CI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,31 @@
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
+})
 
-export default [...compat.extends("eslint:recommended"), {
-    languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.browser,
-        },
+export default [...compat.extends('eslint:recommended'), {
+	languageOptions: {
+		globals: {
+			...globals.node,
+			...globals.browser,
+		},
+		parserOptions: {
+			sourceType: 'module'
+		},
+		ecmaVersion: 12,
+	},
 
-        ecmaVersion: 12,
-        sourceType: "commonjs",
-    },
-
-    rules: {
-        quotes: ["error", "single"],
-        semi: ["error", "never"],
-    },
-}];
+	rules: {
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+	},
+}]
