fdb4e5c9c2de090e318dc29fdc78a3cc05ec29c7
Eduardo Santos <edupaz077@gmail.com>
2025-02-10 14:52:34 -0300

    feat: config database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,31 @@
-import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+
+/* import routes from "./routes/routes.js" */
 import cors from 'cors';
 
 dotenv.config();
-
 const app = express();
+/* app.use(routes) */
+app.use(express.json())
 app.use(cors());
-app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('API funcionando!');
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+/* mongodb+srv://<db_username>:<db_password>@backend.k11ic.mongodb.net/?retryWrites=true&w=majority&appName=Backend */
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@backend.k11ic.mongodb.net/?retryWrites=true&w=majority&appName=Backend`)
+.then(()=>{
+  console.log('Conectou com o mongoDB lá');
+  app.listen(3000, () => {
+    console.log(`Server listening on port 3000`);
+  });
+})
+.catch((err) => {
+  console.error('Erro ao conectar ao banco:', err);
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.get('/', (req, res) => {
+  res.json('opa')
+});
+
