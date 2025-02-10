d0784e1034a14ee0836267e29f6fb789c2c8ed1f
ArtHirche <hirche.arthur@gmail.com>
2025-02-10 14:33:40 -0300

    feat: initial structure of backend

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('API funcionando!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
