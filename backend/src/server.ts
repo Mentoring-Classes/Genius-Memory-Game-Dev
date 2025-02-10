99dcc5ab0cb47ddb18afd8a46760719ec5ad676b
ArtHirche <hirche.arthur@gmail.com>
2025-02-10 15:26:05 -0300

    refactor: format server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,15 +15,15 @@ const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
 /* mongodb+srv://<db_username>:<db_password>@backend.k11ic.mongodb.net/?retryWrites=true&w=majority&appName=Backend */
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@backend.k11ic.mongodb.net/?retryWrites=true&w=majority&appName=Backend`)
-.then(()=>{
-  console.log('Conectou com o mongoDB lá');
-  app.listen(3000, () => {
-    console.log(`Server listening on port 3000`);
+  .then(() => {
+    console.log('Conectou com o mongoDB lá');
+    app.listen(3000, () => {
+      console.log(`Server listening on port 3000`);
+    });
+  })
+  .catch((err) => {
+    console.error('Erro ao conectar ao banco:', err);
   });
-})
-.catch((err) => {
-  console.error('Erro ao conectar ao banco:', err);
-});
 
 app.get('/', (req, res) => {
   res.json('opa')
