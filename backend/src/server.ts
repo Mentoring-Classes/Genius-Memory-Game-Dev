7ac04c538856a64f9beb74a1364edc6b98a7ea09
Eduardo Santos <edupaz077@gmail.com>
2025-02-10 15:56:45 -0300

    refactor: moved route for folder routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,31 +1,28 @@
 import dotenv from 'dotenv';
 import express from "express";
 import mongoose from "mongoose";
+import routes from './routes/routes';
 
 /* import routes from "./routes/routes.js" */
 import cors from 'cors';
 
 dotenv.config();
 const app = express();
-/* app.use(routes) */
 app.use(express.json())
 app.use(cors());
+app.use(routes);
 
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
-/* mongodb+srv://<db_username>:<db_password>@backend.k11ic.mongodb.net/?retryWrites=true&w=majority&appName=Backend */
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@backend.k11ic.mongodb.net/?retryWrites=true&w=majority&appName=Backend`)
   .then(() => {
-    console.log('Conectou com o mongoDB lá');
+    console.log(`Connected to MongoDB with user ${dbUser}`);
+    
     app.listen(3000, () => {
       console.log(`Server listening on port 3000`);
     });
   })
   .catch((err) => {
-    console.error('Erro ao conectar ao banco:', err);
+    console.error('Failed to connect to MongoDB:', err);
   });
-
-app.get('/', (req, res) => {
-  res.json('opa')
-});
 
