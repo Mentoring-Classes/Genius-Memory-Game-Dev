8375617bc11c3567a1ce08e014bc775b1df89ea1
ArtHirche <hirche.arthur@gmail.com>
2025-02-19 16:58:28 -0300

    feat: add Rank model with user reference and ranking system

---
+++ backend/src/models/Rank.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema, Document} from "mongoose";
+
+interface IRank extends Document {
+    userId: mongoose.Types.ObjectId;
+    bestScore: number;
+    rank: String;
+    rankPoints: number;
+    bestStreak: number;  
+}
+
+const RankSchema = new Schema<IRank>({
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
+    bestScore: { type: Number, required: true, default: 0 },
+    rank: { type: String, required: true, default: 'Bronze' },
+    rankPoints: { type: Number, required: true, default: 0 },
+    bestStreak: { type: Number, required: true, default: 0 }
+}, { timestamps: true });
+
+const Rank = mongoose.model<IRank>('Rank', RankSchema);
+export default Rank;
