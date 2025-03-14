474beb32915e9ba830cece5e43ba90b551b440e1
ArtHirche <hirche.arthur@gmail.com>
2025-03-14 13:08:15 -0300

    chore: refactor server setup and integrate winston logger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,29 +1,34 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import routes from './routes';
-import loggerMiddleware from './middlewares/logger';
-
-import cors from 'cors';
+import routes from "./routes";
+import loggerMiddleware from "./middlewares/logger";
+import logger from "./utils/logger";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
-app.use(express.json())
+
+app.use(express.json());
 app.use(cors());
 app.use(loggerMiddleware);
 app.use(routes);
 
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASS
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@backend.k11ic.mongodb.net/?retryWrites=true&w=majority&appName=Backend`)
-  .then(() => {
-    console.log(`Connected to MongoDB with user ${dbUser}`);
+const PORT = process.env.PORT || 3000;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
-    app.listen(3000, () => {
-      console.log(`Server listening on port 3000`);
-    });
+mongoose
+  .connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@backend.k11ic.mongodb.net/?retryWrites=true&w=majority&appName=Backend`
+  )
+  .then(() => {
+    logger.info("Connected to MongoDB");
   })
   .catch((err) => {
-    console.error('Failed to connect to MongoDB:', err);
+    logger.error("Failed to connect to MongoDB:", err);
   });
 
+app.listen(PORT, () => {
+  logger.info(`Server listening on port ${PORT}`);
+});
