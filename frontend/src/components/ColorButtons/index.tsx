6f5dfc92508da98b82775b5adee5dc6c40e469fc
Eduardo Santos <edupaz077@gmail.com>
2025-03-20 15:58:09 -0300

    feat: triggerAnimation for button click

--- frontend/src/components/ColorButtons/index.tsx
+++ frontend/src/components/ColorButtons/index.tsx
@@ -1,19 +1,31 @@
 import { Props } from "./ColorButtonsTypes"
 import './ColorButtons.css'
 
-import gsap from "gsap"
+import { gsap } from "gsap"
 
-const ColorButtons = ({Sequencia}: Props) => {
+const ColorButtons = ({ Sequencia }: Props) => {
+ 
+  const triggerAnimation = (color:string) => {
+    gsap.to(color, {
+      duration: 0.3,
+      ease: "power1.inOut",
+      scale: 1.2,
+      yoyo: true,
+      repeat: 1
+      }
+    )
+  }
+
   return (
     <div className="ColorButtons">
-        <div className='Buttons'>
-            <button className="Red" onClick={() => { Sequencia('Red') }}></button>
-            <button className="Yellow" onClick={() => { Sequencia('Yellow') }}></button>
-        </div>
-        <div className='Buttons'>
-            <button className="Green" onClick={() => { Sequencia('Green') }}></button>
-            <button className="Blue" onClick={() => { Sequencia('Blue') }}></button>
-        </div>
+      <div className='Buttons'>
+        <button className="Red" onClick={() => { Sequencia('Red'),triggerAnimation('.Red') }}></button>
+        <button className="Yellow" onClick={() => { Sequencia('Yellow'), triggerAnimation('.Yellow') }}></button>
+      </div>
+      <div className='Buttons'>
+        <button className="Green" onClick={() => { Sequencia('Green'), triggerAnimation('.Green') }}></button>
+        <button className="Blue" onClick={() => { Sequencia('Blue'), triggerAnimation('.Blue') }}></button>
+      </div>
     </div>
   )
 }
