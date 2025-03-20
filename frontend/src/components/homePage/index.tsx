07325b73ed818fde432550be9e1e29e630fbad3b
Eduardo Santos <edupaz077@gmail.com>
2025-03-20 14:29:57 -0300

    refac: removed tutorial

--- frontend/src/components/homePage/index.tsx
+++ frontend/src/components/homePage/index.tsx
@@ -6,29 +6,6 @@ import ButtonLink from '../buttonLink';
 import './HomePage.css'
 
 const Home = () => {
-	const setupIntro = () => {
-		introJs().setOptions({
-			steps: [
-				{
-					intro: `
-						<div style="text-align: center;">
-							Bem-vindo ao Genius Game 
-						</div>
-					`,
-				},
-				{ element: '.GameModes', intro: 'Escolha seu modo de jogo aqui' },
-				{ element: '#Solo-Game', intro: 'Nesse modo de jogo é igual aos tradicionais' },
-				{ element: '.NaoLogado', intro: 'Para jogar os outros modos, faça login pelo Google' },
-				{ element: '#One-vs-One', intro: 'Nesse modo você pode desafiar outras pessoas' },
-				{ element: '#Co-op', intro: 'Nesse modo você e outro jogador revezam os rounds' },
-			],
-		}).start();
-	};
-
-	useEffect(() => {
-		setupIntro();
-	}, []);
-
 	return (
 		<div className="HomePage">
 			<h1>Genius Game</h1>
