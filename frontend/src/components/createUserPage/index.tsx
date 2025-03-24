e338e624f2c2b391cb12102399c512667129f0aa
kaikeMKY <kaikemassaokamidayamashita@gmail.com>
2025-03-24 16:22:47 -0300

    feat: create user page

--- frontend/src/components/createUserPage/index.tsx
+++ frontend/src/components/createUserPage/index.tsx
@@ -1,33 +1,36 @@
 import {useState} from 'react'
-
-const createUser = () => {
+import "./createUser.css"
+const CreateUser = () => {
   const [email,setEmail] = useState("")
   const [password,setPassword] = useState("")
   
     return (
-    <div>
-      <h1>Cadastrar Usuário</h1>
+    <div className='create-user-container'>
+      <h1 className='create-user-title'>Cadastrar Usuário</h1>
+      <form className="create-user-form">
+
         <div>
             <input
              type="email"
              onChange={(e)=>setEmail(e.target.value)}
              placeholder='SeuEmail@email.com'
-              />
+             />
         </div>
         <div>
             <input
              type="password"
              onChange={(e)=>setPassword(e.target.value)}
              placeholder='Digite Sua Senha'
-              />
+             />
         </div>
         <button
         type='submit'
-        >
+        > enviar
         
         </button>
+   </form>
     </div>
   )
 }
 
-export default createUser
+export default CreateUser
