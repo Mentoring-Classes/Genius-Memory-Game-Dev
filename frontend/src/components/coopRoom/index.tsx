cee84bfcd8e7232f0ff08abb98c01e3b08398050
kaikeMKY <kaikemassaokamidayamashita@gmail.com>
2025-04-03 16:24:08 -0300

    style: coopRoom h1,p to white

--- frontend/src/components/coopRoom/index.tsx
+++ frontend/src/components/coopRoom/index.tsx
@@ -2,19 +2,19 @@
 import { useState } from "react";
 import RoomForm from "../roomForm";
 import ButtonLink from "../buttonLink";
-
+import "./coopRoom.css";
 
 const CoopRoom = () => {
   const [isAuth] = useState<string | null>(localStorage.getItem("token") || null);
   const [userId] = useState<string | null>(localStorage.getItem("userId"));
 
   return (
-    <div>
+    <div className="coopRoom">
       <h1>Cooperativo</h1>
       {isAuth && userId? (
           <RoomForm />
       ) : (
-        <div>
+        <div className="notLoggedIn">
           <p>Faça login primeiro</p>
           <ButtonLink buttontext="Voltar" to="/"></ButtonLink>
         </div>
